f879d90e3a2300937b88535dff650af7e1e69e09
Markus <markus.mork@iths.se>
2022-02-16 11:36:48 +0100

    init

---
+++ src/store/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as API from '@/API'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    authData: null,
+    posts: [],
+  },
+  mutations: {
+    saveAuthData(state, data){
+      state.authData=data
+    },
+    showPosts(state, posts){
+      state.posts = posts
+    }
+  },
+  actions: {
+    async authenticate(context, credentials){
+      const response = await API.login(
+        credentials.email, credentials.password
+      )
+      API.saveToken(response.data.token)
+
+      context.commit('saveAuthData', response.data)
+    },
+    async showPosts(context){
+      const response = await API.getPosts()
+      context.commit('showPosts', response)
+    }
+  },
+})
+
+//Token sparas i axios och behöver inte skickas med till API.getPosts
+// context.state.authData.token
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <form action="">
+      <input type="text" placeholder="email" v-model="email">
+      <input type="password" placeholder="password" v-model="password">
+    </form>
+    <button @click="login">Login</button>
+    <button v-if="authData" @click="showPosts">toggle posts</button>
+    {{authData}}
+    <div class="posts" v-if="printPosts">{{posts.data.posts[0].content}}</div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {return{
+    email: 'darrin.lebsack@yahoo.com',
+    password: 'grillkorv',
+    printPosts: false,
+  } 
+  },
+  computed:{
+    authData(){
+      return this.$store.state.authData
+    },
+    posts(){
+      
+      return this.$store.state.posts
+    }
+  },
+  methods:{
+    login(){
+      this.$store.dispatch('authenticate', {email: this.email, password: this.password}) 
+    },
+    showPosts(){
+      this.printPosts = !this.printPosts
+      this.$store.dispatch('showPosts') 
+    }
+  }
+}
+</script>
